d5b27e70f1539109017840f69046a0fc7d930398
loguefx <loguefx@users.noreply.github.com>
2026-02-02 18:06:32 -0800

    Fix build error - add System.Linq using

--- MetadataRenamer/Jellyfin.Plugin.Template/Services/RenameCoordinator.cs
+++ MetadataRenamer/Jellyfin.Plugin.Template/Services/RenameCoordinator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Jellyfin.Plugin.MetadataRenamer.Configuration;
 using MediaBrowser.Controller.Entities.TV;
 using MediaBrowser.Controller.Library;
